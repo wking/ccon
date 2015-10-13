d866dea22ae1b85e4eea90d3eba7d93a33bc94ad
W. Trevor King <wking@tremily.us>
2015-10-13 09:55:02 -0700

    ccon: Load config and stub in validation
    Using json_load_file [1], JSON_REJECT_DUPLICATES [2], json_is_object
    [3], and json_decref [4].
    
    [1]: https://jansson.readthedocs.org/en/2.7/apiref.html#c.json_load_file
    [2]: https://jansson.readthedocs.org/en/2.7/apiref.html#decoding
    [3]: https://jansson.readthedocs.org/en/2.7/apiref.html#c.json_is_object
    [4]: https://jansson.readthedocs.org/en/2.7/apiref.html#c.json_decref

--- ccon.c
+++ ccon.c
@@ -1,6 +1,41 @@
+#include <stdio.h>
+
 #include <jansson.h>
+
+int validate_config(json_t * config)
+{
+	if (!json_is_object(config)) {
+		fprintf(stderr, "config JSON is not an object\n");
+		return 1;
+	}
+	// TODO: actually validate the data
+	return 0;
+}
 
 int main(int argc, char **argv)
 {
-	return 0;
+	char *config_path = "config.json";
+	int err;
+	json_t *config;
+	json_error_t error;
+
+	config = json_load_file(config_path, JSON_REJECT_DUPLICATES, &error);
+	if (!config) {
+		fprintf(stderr, "error on %s:%d: %s\n", config_path, error.line,
+			error.text);
+		return 1;
+	}
+
+	err = validate_config(config);
+	if (err) {
+		fprintf(stderr, "%s invalid\n", config_path);
+		goto cleanup;
+	}
+
+ cleanup:
+	if (config) {
+		json_decref(config);
+	}
+
+	return err;
 }
